6aa0b55b0669ebfc40a454ed1978c747959280d2
Peiyi Xu <xu_py311@outlook.com>
2025-12-05 00:05:44 -0500

    finished exercise 6.2

---
+++ chapter6/exercise2.fsx
@@ -0,0 +1,12 @@
+let drive distance gas = 
+    if distance > 50.0 then gas / 2.0
+    elif distance > 25.0 then gas - 10.0
+    elif distance > 0.0 then gas - 1.0
+    else gas
+
+
+let remaining = 
+    100.0
+    |> drive 55
+    |> drive 26
+    |>drive 1
